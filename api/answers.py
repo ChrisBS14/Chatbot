c9930ef899e8ab41ebf3552c27a372d1d44865b7
invictuus <niklas.adler@outlook.com>
2022-09-28 20:48:46 +0200

    commit: - Cleanup of some code - Reworded some phrases - Added new answers - Removed some unfinished code - Edited some comments

--- api/answers.py
+++ api/answers.py
@@ -30,40 +30,50 @@ def answers():
 
     # if the answer is help
     if answer.lower() == "help":
-        help = "You can choose between these topics 'Teamleader', 'CEO', 'Support', 'HelpDesk', 'Invoice' or 'Ticket-Status'"
+        help = "You can choose between these topics 'Teamleader', 'CEO', 'HelpDesk', 'Invoice', 'Location' or 'Application'"
         return payload_helper(help)
 
-    # add the answer 'chris' for the user replies
+    # add the answer 'invoice' for the user replies
     if answer.lower() == "invoice":
-        invoice = "Contact data from Hubert Gröninger - Team Leader IT 1st-Level : Telefon: 040/69 69 403, E-Mail:hgroeninger@sit.com"
+        invoice = "Contact information of Hubert Gröninger - Team Leader IT 1st-Level : Tel: 040/69 69 403, E-Mail:hgroeninger@itsolutions555.com"
         return payload_helper(invoice)
 
-    # add the answer 'ceo' for the user replies
+    # add the answer 'CEO' for the user replies
     if answer.lower() == "ceo":
-        CEO = "Contact data of CEO Peter Falter: Telefon: 040/69 69 303, E-Mail:pfalter@sit.com"
-        return payload_helper(CEO)
+        ceo = "Contact information of CEO Peter Falter: Tel: 040/69 69 303, E-Mail:pfalter@itsolutions555.com"
+        return payload_helper(ceo)
 
     # add the answer 'Teamleader' for the user replies
     if answer.lower() == "teamleader":
-        Teamleader = "If you want to call them: 040/6969503 or send a Mail to teamleader@sit.com"
-        return payload_helper(Teamleader)
+        teamleader = "If you want to call them: 040/6969503 or send a Mail to teamleader@itsolutions555.com. If you want to reach a specific support level type in 'level'"
+        return payload_helper(teamleader)
 
-    # add the answer 'level' for the user replies
+    # add the answer 'Level' for the user replies
     if answer.lower() == "level":
-        Level = "If you want to see an overview about all Departments type in 'level'"
-        return payload_helper(Level)
+        level = "First Level: helpdesk@itsolutions555.com, 2nd Level: serverteam@itsolutions555.com, 3rd Level: datacenter@itsolutions555.com"
+        return payload_helper(level)
 
     # add the answer 'HelpDesk' for the user replies
     if answer.lower() == "helpdesk":
-        HelpDesk = "If you want to open a Ticket send a Mail to support@sit.com or call them at 040/6969603"
-        return payload_helper(HelpDesk)
+        helpdesk = "If you want to open a Ticket send a Mail to helpdesk@itsolutions555.com or call them at 040/6969603"
+        return payload_helper(helpdesk)
+
+    # add the answer 'Location' for the user replies
+    if answer.lower() == "location":
+        location = "Friedrich-Ebert-Damm 311, 22159 Hamburg"
+        return payload_helper(location)
+
+    # add the answer 'Application' for the user replies
+    if answer.lower() == "application":
+        application = "For applications via mail please send it to Mr. Neutron, Friedrich-Ebert-Damm 311, 22159 Hamburg or via e-Mail: jimmy.neutron@itsolutions555.com for further requirements regarding the job offer please visit our website https://itsolutions555.com/jobapplications/requirements"
+        return payload_helper(application)
 
     # if the answer is bye
     if answer.lower() == "bye":
         return payload_helper("bye bye")
 
     # if the answer is not in the user replies send a basic robot answer
-    payload = payload_helper("You can choose between these topics 'Teamleader', 'CEO', 'Support', 'HelpDesk', 'Invoice' or 'Ticket-Status'" )
+    payload = payload_helper("You can choose between these topics 'Teamleader', 'CEO', 'HelpDesk', 'Invoice' or 'Location' or 'Application'" )
 
     # return the JSON payload
     return payload
