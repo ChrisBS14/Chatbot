1a39068792f262c5c2caf0943d91e1b2125d9c5b
invictuus <niklas.adler@outlook.com>
2022-09-28 19:17:15 +0200

    - Fixed typos - Added a few more variations to hello

--- database/chatbot_replies.py
+++ database/chatbot_replies.py
@@ -2,4 +2,4 @@
 no_text = ["I'm sorry, I didn't understand that. Please try again.", "Could you repeat that?", "I didn't quite catch that. Could you repeat that?"]
 
 # if the user input is in user_replies, the chatbot should answer with a random answer from the array answers_to_hello
-answers_to_hello = ["Hello", "Hey", "Hey-ho", "Hi","Hi there"]
+answers_to_hello = ["Hello", "Hey", "Hey ho", "Hi","Hi there"]
